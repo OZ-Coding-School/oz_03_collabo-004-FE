13f26037bb35102a4bd1ea020bb4837ef539b99d
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-08-07 14:22:42 +0900

    Chore: 스테이징 페이지 테스트

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -3,6 +3,7 @@ import AuthComponent from "../components/AuthComponent";
 const LoginPage = () => {
     return (
         <>
+            <div>스테이징 페이지</div>
             <AuthComponent type="LOGIN" />
         </>
     );

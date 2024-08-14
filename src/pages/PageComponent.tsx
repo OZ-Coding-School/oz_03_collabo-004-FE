2d6974b499f03b6d721a00c338ea4121aff9bae6
srnnnn <tpfla311@naver.com>
2024-08-14 14:49:11 +0900

    Feat: Common 전문 tag

--- src/pages/PageComponent.tsx
+++ src/pages/PageComponent.tsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import TagSkill from "../common/tag/TagSkill";
+import { FaGamepad } from "react-icons/fa";
+import InfoMyPageLeft from "../common/info/InfoMyPageLeft";
 
 const PageComponent = () => {
+    const dummyTags = [
+        { id: 0, icon: <FaGamepad />, text: "연애 훈수" },
+        { id: 1, icon: <FaGamepad />, text: "집안일 훈수" },
+        { id: 2, icon: <FaGamepad />, text: "고민 훈수" },
+        { id: 3, icon: <FaGamepad />, text: "소소 훈수" },
+        { id: 4, icon: <FaGamepad />, text: "상상 훈수" },
+        { id: 5, icon: <FaGamepad />, text: "패션 훈수" },
+        { id: 6, icon: <FaGamepad />, text: "게임 훈수" },
+        { id: 7, icon: <FaGamepad />, text: "교육 훈수" },
+    ];
     return (
         <div className="select-none font-default bg-black w-full h-[100vh] p-10">
             <Link
@@ -10,6 +23,14 @@ const PageComponent = () => {
                 돌아가기
             </Link>
             ;
+            <div className="bg-white p-2 m-2 flex flex-wrap gap-2">
+                {dummyTags.map((tag) => (
+                    <TagSkill key={tag.id} tagIcon={tag.icon} tagText={tag.text} />
+                ))}
+            </div>
+            <div className="mt-2">
+                <InfoMyPageLeft />
+            </div>
         </div>
     );
 };

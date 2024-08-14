db96bf2a043265cdb93e3196d24239e6f12141f4
akwjr963 <akwjr963@daum.net>
2024-08-14 17:28:44 +0900

    Feat: WriteModal 컴포넌트 작성완료

---
+++ src/common/writeModal/WriteModal.tsx
@@ -0,0 +1,18 @@
+import { RiEditBoxLine } from "react-icons/ri";
+
+interface WriteModalProps {
+    onClick?: () => void;
+}
+
+const WriteModal = ({ onClick }: WriteModalProps) => {
+    return (
+        <button onClick={onClick} className="bg-gray-100 border-none rounded-[5px] py-2 px-4 text-left w-[676px]">
+            <div className="text-gray-500 flex justify-between items-center h-[20px]">
+                <span>|</span>
+                <RiEditBoxLine className="text-literal-confirm h-[20px]" />
+            </div>
+        </button>
+    );
+};
+
+export default WriteModal;

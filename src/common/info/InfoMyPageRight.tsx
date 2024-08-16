665061383b107bc4f7d8df9a6ab0b5b800cbced4
srnnnn <tpfla311@naver.com>
2024-08-16 14:16:46 +0900

    Design: 마이페이지 오른쪽 섹션 전문태그 부분 추가

--- src/common/info/InfoMyPageRight.tsx
+++ src/common/info/InfoMyPageRight.tsx
@@ -1,14 +1,31 @@
 import { IoMdArrowDropdown } from "react-icons/io";
+import Button from "../button/Button";
+import { useState } from "react";
+import { FaGamepad } from "react-icons/fa";
+import TagSkill from "../tag/TagSkill";
 
 const InfoMyPageRight = () => {
+    const [isEdit, setIsEdit] = useState<boolean>(false);
     const progress = 15;
+    const len = 0;
+    const dummyTags = [
+        { id: 0, icon: <FaGamepad />, text: "연애 훈수" },
+        { id: 1, icon: <FaGamepad />, text: "집안일 훈수" },
+        { id: 2, icon: <FaGamepad />, text: "고민 훈수" },
+        { id: 3, icon: <FaGamepad />, text: "소소 훈수" },
+        { id: 4, icon: <FaGamepad />, text: "상상 훈수" },
+        { id: 5, icon: <FaGamepad />, text: "패션 훈수" },
+        { id: 6, icon: <FaGamepad />, text: "게임 훈수" },
+        { id: 7, icon: <FaGamepad />, text: "교육 훈수" },
+    ];
+
     return (
         <div className="bg-white max-w-[780px] min-h-[270px] rounded-2xl py-5 px-10 flex-col">
             <div className="flex mb-4">
                 <div className="font-point font-bold text-lg text-literal-normal">훈수 레벨 : LV 1</div>
-                <div className="flex flex-col items-end text-gray-700 font-default text-xs ml-auto">
-                    <p>채택된 훈수 1개</p>
-                    <p>받은 경고 1건</p>
+                <div className="flex flex-col items-end font-default text-xs ml-auto">
+                    <p className="text-literal-normal">채택된 훈수 1개</p>
+                    <p className="text-literal-highlight">받은 경고 1건</p>
                 </div>
             </div>
             <div className="w-full h-[42px]">
@@ -26,8 +43,18 @@ const InfoMyPageRight = () => {
                     ></div>
                 </div>
             </div>
-            <div className="flex flex-col mt-9">
-                <p className="font-point text-base text-literal-normal mb-4">나의 전문 훈수는</p>
+            <div className="flex mt-9  mb-4 justify-between">
+                <p className="font-point text-base text-literal-normal">나의 전문 훈수는</p>
+                <Button onClick={() => setIsEdit(!isEdit)} className={"text-xs w-13 h-6 py-1"}>
+                    {isEdit ? "완료" : "수정"}
+                </Button>
+            </div>
+            <div className="flex flex-wrap  gap-3">
+                {isEdit ? (
+                    dummyTags.map((tag) => <TagSkill key={tag.id} tagIcon={tag.icon} tagText={tag.text} />)
+                ) : len <= 0 ? (
+                    <div className="font-default text-sm text-center w-full my-2">나만의 훈수 태그를 골라보세요!</div>
+                ) : null}
             </div>
         </div>
     );
--- src/common/tag/TagSkill.tsx
+++ src/common/tag/TagSkill.tsx
@@ -16,7 +16,7 @@ const TagSkill = ({ tagIcon, tagText }: TagSkillProps) => {
         <div
             onClick={handleClick}
             className={tw(
-                "w-[150px] h-[40px] flex rounded-full py-2 px-5 cursor-pointer gap-1",
+                "min-w-[150px] max-h-[40px] flex rounded-full py-2 px-5 cursor-pointer gap-1",
                 isClicked ? "bg-primary-background" : " border-primary-background border"
             )}
         >

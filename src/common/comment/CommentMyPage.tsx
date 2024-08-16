7fa47d1ad704b331a4ec9cdd541e690429a95c42
srnnnn <tpfla311@naver.com>
2024-08-16 16:43:41 +0900

    Design: 일단 탭에 따라 ui가 달라지도록 수정

--- src/common/comment/CommentMyPage.tsx
+++ src/common/comment/CommentMyPage.tsx
@@ -1,8 +1,11 @@
 const CommentMyPage = () => {
     return (
-        <div className="bg-gray-100 rounded-2xl">
-            <p className="text-literal-normal text-sm py-4 px-6">이야기 잘봤음</p>
-        </div>
+        <>
+            <p className="font-default text-sm text-gray-600 font-normal p-2">미채택</p>
+            <div className="bg-gray-100 rounded-2xl">
+                <p className="text-literal-normal text-sm py-4 px-6">이야기 잘봤음</p>
+            </div>
+        </>
     );
 };
 
--- src/common/content/ContentMyPage.tsx
+++ src/common/content/ContentMyPage.tsx
@@ -2,21 +2,21 @@ import Badge from "../badge/Badge";
 import CommentMyPage from "../comment/CommentMyPage";
 
 const ContentMyPage = () => {
+    const tab = true;
     return (
         <div className="max-w-[780px]">
             <div className="flex gap-2 mb-3 ml-2">
-                <p className="text-sm font-medium mr-4 text-literal-normal">따지고 보면 잔소리라 생각되진 않아</p>
+                {tab && (
+                    <p className="text-sm font-medium mr-4 text-literal-normal">따지고 보면 잔소리라 생각되진 않아</p>
+                )}
                 <p className="text-sm text-gray-600 mr-2 font-default">2024년 08월 12일</p>
                 <Badge color="yellow">고민</Badge>
                 <Badge color="yellow">소소</Badge>
             </div>
             <div className="bg-white px-6 py-4 w-full rounded-2xl">
                 <p className="text-lg font-default text-literal-normal pb-3 border-b border-b-gray-100">고민내용</p>
-                <p className="font-default text-sm text-gray-600 font-normal p-2">미채택</p>
-                {/* <div className="bg-gray-100 rounded-2xl">
-                    <p className="text-literal-normal text-sm py-4 px-6">이야기 잘봤음</p>
-                </div> */}
-                <CommentMyPage />
+
+                {tab ? <CommentMyPage /> : <p className="my-3 text-literal-normal text-sm">고민내용내용내용</p>}
             </div>
         </div>
     );

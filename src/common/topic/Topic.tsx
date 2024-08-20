6b9c97996236d1784218479b0d527156203fca8b
serim <tpfla311@naver.com>
2024-08-20 16:38:02 +0900

    Design: 토픽 메뉴바 드롭다운 애니메이션 추가

--- src/common/topic/Topic.tsx
+++ src/common/topic/Topic.tsx
@@ -3,6 +3,26 @@ import { twMerge as tw } from "tailwind-merge";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { IoMdArrowDropup } from "react-icons/io";
 import { TOPIC_TAGS } from "../../config/const";
+import { motion, Variants } from "framer-motion";
+
+const dropdownVariants: Variants = {
+    open: {
+        height: "auto",
+        transition: {
+            type: "spring",
+            bounce: 0,
+            duration: 0.3,
+        },
+    },
+    closed: {
+        height: 0,
+        transition: {
+            type: "spring",
+            bounce: 0,
+            duration: 0.3,
+        },
+    },
+};
 
 const Topic = () => {
     const [isDailyOpen, setIsDailyOpen] = useState(true);
@@ -12,7 +32,7 @@ const Topic = () => {
     return (
         <div className="w-[190px] bg-white h-auto p-2 rounded-md font-default ">
             <p className="font-medium text-lg text-primary-second-dark px-1 py-2">카테고리</p>
-            <div className="w-full mb-3">
+            <div className="w-full mb-2">
                 <button
                     onClick={() => setIsDailyOpen(!isDailyOpen)}
                     className={tw(
@@ -22,17 +42,20 @@ const Topic = () => {
                 >
                     일상 {isDailyOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
                 </button>
-                {isDailyOpen && (
-                    <ul className="text-sm pl-2 space-y-2 text-literal-normal">
-                        {TOPIC_TAGS.slice(0, 6).map((i) => (
-                            <li className="flex items-center gap-2 hover:text-primary-second-dark cursor-pointer">
-                                {i.icon} {i.text}
-                            </li>
-                        ))}
-                    </ul>
-                )}
+                <motion.ul
+                    initial={false}
+                    animate={isDailyOpen ? "open" : "closed"}
+                    variants={dropdownVariants}
+                    className="overflow-hidden text-sm pl-2 space-y-2 text-literal-normal"
+                >
+                    {TOPIC_TAGS.slice(0, 6).map((i) => (
+                        <li className="flex items-center gap-2 duration-150 hover:text-primary-second-dark cursor-pointer">
+                            {i.icon} {i.text}
+                        </li>
+                    ))}
+                </motion.ul>
             </div>
-            <div className="w-full mb-3">
+            <div className="w-full mb-2">
                 <button
                     onClick={() => setIsGameOpen(!isGameOpen)}
                     className={tw(
@@ -42,17 +65,20 @@ const Topic = () => {
                 >
                     게임 {isGameOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
                 </button>
-                {isGameOpen && (
-                    <ul className="text-sm pl-2 space-y-2 text-literal-normal">
-                        {TOPIC_TAGS.slice(6, 8).map((i) => (
-                            <li className="flex items-center gap-2 hover:text-primary-second-dark cursor-pointer">
-                                {i.icon} {i.text}
-                            </li>
-                        ))}
-                    </ul>
-                )}
+                <motion.ul
+                    initial={false}
+                    animate={isGameOpen ? "open" : "closed"}
+                    variants={dropdownVariants}
+                    className="overflow-hidden text-sm pl-2 space-y-2 text-literal-normal"
+                >
+                    {TOPIC_TAGS.slice(6, 8).map((i) => (
+                        <li className="flex items-center gap-2 duration-150 hover:text-primary-second-dark cursor-pointer">
+                            {i.icon} {i.text}
+                        </li>
+                    ))}
+                </motion.ul>
             </div>
-            <div className="w-full mb-3">
+            <div className="w-full mb-2">
                 <button
                     onClick={() => setIsEduOpen(!isEduOpen)}
                     className={tw(
@@ -61,16 +87,19 @@ const Topic = () => {
                     )}
                 >
                     교육 {isEduOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-                </button>
-                {isEduOpen && (
-                    <ul className="text-sm pl-2 space-y-2 text-literal-normal">
-                        {TOPIC_TAGS.slice(8, 9).map((i) => (
-                            <li className="flex items-center gap-2 hover:text-primary-second-dark cursor-pointer">
-                                {i.icon} {i.text}
-                            </li>
-                        ))}
-                    </ul>
-                )}
+                </button>{" "}
+                <motion.ul
+                    initial={false}
+                    animate={isEduOpen ? "open" : "closed"}
+                    variants={dropdownVariants}
+                    className="overflow-hidden text-sm pl-2 space-y-2 text-literal-normal"
+                >
+                    {TOPIC_TAGS.slice(8, 9).map((i) => (
+                        <li className="flex items-center gap-2 duration-150 hover:text-primary-second-dark cursor-pointer">
+                            {i.icon} {i.text}
+                        </li>
+                    ))}
+                </motion.ul>
             </div>
         </div>
     );

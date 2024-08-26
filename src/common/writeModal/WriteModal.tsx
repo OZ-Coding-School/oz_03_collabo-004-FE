d488b2bcf6a38ad4509fcb9b3fbb7b26ce3a1aff
serim <tpfla311@naver.com>
2024-08-26 18:09:27 +0900

    Feat: input 태그 추가 및 이벤트 설정, 디자인 수정

--- src/common/writeModal/WriteModal.tsx
+++ src/common/writeModal/WriteModal.tsx
@@ -1,17 +1,52 @@
-import { RiEditBoxLine } from "react-icons/ri";
+import { FaArrowCircleUp } from "react-icons/fa";
+import { useEffect, useRef, useState } from "react";
 
 interface WriteModalProps {
-    onClick?: () => void;
+    onClose: () => void;
 }
 
-const WriteModal = ({ onClick }: WriteModalProps) => {
+const WriteModal = ({ onClose }: WriteModalProps) => {
+    const writeRef = useRef<HTMLInputElement>(null);
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        if (writeRef.current) {
+            writeRef.current.focus();
+        }
+    }, []);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("훈수글: ", text);
+        setText("");
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === "Escape") {
+            onClose();
+            return;
+        }
+        if (e.key === "Enter") {
+            handleSubmit(e);
+            return;
+        }
+    };
     return (
-        <button onClick={onClick} className="bg-gray-100 border-none rounded-[5px] py-2 px-4 text-left w-full">
-            <div className="text-gray-500 flex justify-between items-center h-[20px]">
-                <span>|</span>
-                <RiEditBoxLine className="text-literal-confirm h-[20px]" />
-            </div>
-        </button>
+        <form className="bg-gray-100 border-none rounded-[5px] py-2 px-4 text-left w-full flex">
+            <input
+                className="bg-transparent w-full focus:outline-none py-1"
+                ref={writeRef}
+                onKeyDown={handleKeyDown}
+                placeholder="훈수를 작성해주세요."
+                maxLength={150}
+                value={text}
+                onChange={(e) => setText(e.currentTarget.value)}
+            />
+            <FaArrowCircleUp
+                onClick={handleSubmit}
+                className="my-auto size-5 cursor-pointer text-primary-background-second hover:text-primary-background"
+            />
+        </form>
     );
 };
 

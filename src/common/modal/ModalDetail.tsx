6b2d5acc5956698d8c270a7e714c2e5ca7b1106b
serim <tpfla311@naver.com>
2024-08-26 14:21:27 +0900

    Feat: 디테일모달에 필요한 컴포넌트 합치기

--- src/common/modal/ModalDetail.tsx
+++ src/common/modal/ModalDetail.tsx
@@ -2,7 +2,9 @@ import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { IoClose } from "react-icons/io5";
 import { ModalProps } from "../../config/types";
-import Button from "../button/Button";
+import Badge from "../badge/Badge";
+import WriteModal from "../writeModal/WriteModal";
+import Comment from "../comment/Comment";
 
 const ModalDetail = ({ onClose, isOpen, parent }: ModalProps) => {
     useEffect(() => {
@@ -32,30 +34,45 @@ const ModalDetail = ({ onClose, isOpen, parent }: ModalProps) => {
             ></motion.nav>
             <div
                 onClick={onClose}
-                className="text-literal-normal inset-0 font-default z-40 fixed flex items-center justify-center"
+                className="text-literal-normal inset-0 font-default z-40 fixed flex items-center justify-center md:px-2"
             >
                 <motion.nav
                     initial={{ opacity: 0, translateY: 20 }}
                     animate={{ opacity: [1], translateY: 0 }}
                     exit={{ opacity: 0 }}
                     onClick={(e) => e.stopPropagation()}
-                    className="outline-none w-full h-full md:w-[570px] md:h-[584px] md:rounded-3xl bg-white relative flex justify-center items-center"
+                    className="outline-none w-full h-full md:w-[870px] md:max-h-[650px] md:rounded-3xl bg-white relative py-10 px-14"
                 >
-                    <form className="md:px-[80px] flex flex-col justify-center items-center">
-                        <div className="w-full font-bold text-lg font-point text-center pt-10">훈수 신고</div>
+                    <div className="flex flex-col gap-2">
+                        <div className="flex justify-between">
+                            <div className="flex gap-2">
+                                <Badge color="yellow">고민</Badge>
+                                <Badge color="yellow">소소</Badge>
+                            </div>
+                            <div className="text-sm text-gray-500">수정 / 삭제</div>
+                        </div>
+                        <div className="text-gray-500 text-sm">2024년 08월 26일</div>
+                        <div className="text-xl my-2">
+                            비전공자, 전문대졸..이었던 내가 지금은 풀스택개발자입니다.. 고민상담
+                        </div>
+                        <div className="mt-3 mb-10">
+                            <p>내용들..</p>
+                        </div>
+                        <form className="my-5">
+                            <WriteModal />
+                        </form>
+                        <div>
+                            <Comment />
+                        </div>
+
+                        {/* <div className="w-full font-bold text-lg font-point text-center pt-10">훈수 신고</div>
                         <div className="w-full font-default text-center mt-5 text-literal-error">
                             부적절한 신고는 다른 사용자에게 불필요한 피해를 줄 수 있습니다.
                         </div>
                         <div className="w-full font-default text-center text-literal-error">
                             신고 사유를 신중하게 작성해 주시기 바랍니다.
-                        </div>
-                        <div className="mt-10 relative w-full h-[226px] flex">
-                            <div className="absolute bottom-2 right-2 text-gray-600">{length}자</div>
-                        </div>
-                        <Button className="w-full mt-10" color="danger">
-                            신고하기
-                        </Button>
-                    </form>
+                        </div> */}
+                    </div>
                     <IoClose
                         onClick={onClose}
                         title="닫기"

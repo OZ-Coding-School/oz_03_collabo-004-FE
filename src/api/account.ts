633873334c42a05653f7c1e625899cb492dc5384
serim <tpfla311@naver.com>
2024-08-27 18:48:39 +0900

    Feat: 이미지 수정  api 추가

--- src/api/account.ts
+++ src/api/account.ts
@@ -1,9 +1,19 @@
 import { axiosInstance } from "./axios";
+import { UserUpdateData } from "../config/types";
 
 export const userInfo = () => {
     return axiosInstance.get("/account/profile/", { withCredentials: true });
 };
 
-export const userInfoUpdate = () => {
-    return axiosInstance.put("/account/profile/update/", { withCredentials: true });
+export const userInfoUpdate = (updateData: UserUpdateData) => {
+    return axiosInstance.put("/account/profile/update/", updateData, { withCredentials: true });
+};
+
+export const userInfoImageUpdate = (image: File) => {
+    const formData = new FormData();
+    formData.append("profile_image", image);
+    return axiosInstance.put("/account/profile/image/", formData, {
+        withCredentials: true,
+        headers: { "Content-Type": "multipart/form-data" },
+    });
 };

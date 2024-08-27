79cf90004d0a525a3c93cb48f7587f0bcfd2b499
serim <tpfla311@naver.com>
2024-08-27 13:30:43 +0900

    Design: user 이미지 가져옴

--- src/common/writeModal/WriteModal.tsx
+++ src/common/writeModal/WriteModal.tsx
@@ -1,5 +1,6 @@
 import { FaArrowCircleUp } from "react-icons/fa";
 import { useEffect, useRef, useState } from "react";
+import ProfileImage from "../profile/ProfileImage";
 
 interface WriteModalProps {
     onClose: () => void;
@@ -32,21 +33,26 @@ const WriteModal = ({ onClose }: WriteModalProps) => {
         }
     };
     return (
-        <form className="bg-gray-100 border-none rounded-[5px] py-2 px-4 text-left w-full flex">
-            <input
-                className="bg-transparent w-full focus:outline-none py-1"
-                ref={writeRef}
-                onKeyDown={handleKeyDown}
-                placeholder="훈수를 작성해주세요."
-                maxLength={150}
-                value={text}
-                onChange={(e) => setText(e.currentTarget.value)}
-            />
-            <FaArrowCircleUp
-                onClick={handleSubmit}
-                className="my-auto size-5 cursor-pointer text-primary-background-second hover:text-primary-background"
-            />
-        </form>
+        <div className="flex gap-2">
+            <div className="size-11 my-auto">
+                <ProfileImage />
+            </div>
+            <form className="bg-gray-100 border-none rounded-[5px] py-2 px-4 text-left w-full flex">
+                <input
+                    className="bg-transparent w-full focus:outline-none py-1"
+                    ref={writeRef}
+                    onKeyDown={handleKeyDown}
+                    placeholder="훈수를 작성해주세요."
+                    maxLength={150}
+                    value={text}
+                    onChange={(e) => setText(e.currentTarget.value)}
+                />
+                <FaArrowCircleUp
+                    onClick={handleSubmit}
+                    className="my-auto size-5 cursor-pointer text-primary-background-second hover:text-primary-background"
+                />
+            </form>
+        </div>
     );
 };
 

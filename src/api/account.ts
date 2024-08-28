85f44a03ec8267a250d71f0cc9f2cde502a3cb31
serim <tpfla311@naver.com>
2024-08-28 20:28:33 +0900

    Feat: api 추가

--- src/api/account.ts
+++ src/api/account.ts
@@ -17,3 +17,8 @@ export const userInfoImageUpdate = (image: File) => {
         headers: { "Content-Type": "multipart/form-data" },
     });
 };
+
+//다른 유저의 마이페이지 볼 수 있는
+export const userInfoPublic = (userId: string) => {
+    return axiosInstance.get(`/account/profile/${userId}/`);
+};

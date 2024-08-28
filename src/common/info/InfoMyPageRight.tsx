2ed99248695594e43ea1af7e9e28a1056b06d62f
serim <tpfla311@naver.com>
2024-08-28 20:22:13 +0900

    Fix: 태그 관련 코드 수정

--- src/common/info/InfoMyPageRight.tsx
+++ src/common/info/InfoMyPageRight.tsx
@@ -7,14 +7,19 @@ import { useToastStore, useUserStore } from "../../config/store";
 import Toast from "../../common/toast/Toast";
 import { userInfoUpdate } from "../../api/account";
 
-const InfoMyPageRight = () => {
+type InfoMyPageLeftProps = {
+    isUserMypage: boolean;
+};
+
+const InfoMyPageRight = ({ isUserMypage }: InfoMyPageLeftProps) => {
     const { toast, setToast } = useToastStore();
     const { user } = useUserStore();
     const [isEdit, setIsEdit] = useState<boolean>(false);
     const [selectedTags, setSelectedTags] = useState<number[]>([]);
 
     const len = selectedTags.length;
-    const progress = 0;
+    const progress = 94;
+    const otheruser = "따지고 보면 잔소리라 생각되진 않아";
 
     useEffect(() => {
         setSelectedTags(user.selected_tags);
@@ -36,19 +41,20 @@ const InfoMyPageRight = () => {
     };
 
     const handleTagUpdate = async () => {
+        console.log(selectedTags);
         await userInfoUpdate({ selected_tags: selectedTags });
         setIsEdit(false);
     };
 
     return (
-        <div className="bg-white w-full min-w-[300px] rounded-2xl py-6 px-10 flex-col">
+        <div className="bg-white w-full min-w-[300px] md:mt-10 rounded-2xl py-6 px-10 flex-col">
             {toast.status && <Toast />}
             <div className="flex mb-6">
                 <div className="font-point font-bold text-lg text-literal-normal">
                     훈수 레벨 : LV {user.hunsoo_level}
                 </div>
                 <div className="flex flex-col items-end font-default text-xs ml-auto">
-                    <p className="text-literal-normal">채택된 훈수 {user.selected_comment_count}개</p>
+                    {isUserMypage && <p className="text-literal-normal">채택된 훈수 {user.selected_comment_count}개</p>}
                     <p className="text-literal-highlight">받은 경고 {user.warning_count}건</p>
                 </div>
             </div>
@@ -57,7 +63,9 @@ const InfoMyPageRight = () => {
                     className="flex flex-col items-center rounded-full transition-all duration-300"
                     style={{ width: `${progress * 2}%` }}
                 >
-                    <p className="font-default text-sm text-primary-second-dark min-w-[60px] text-center">훈수 새싹</p>
+                    <p className="font-default text-sm font-medium text-primary-second-dark min-w-[60px] text-center">
+                        궁극의 훈수꾼{" "}
+                    </p>
                     <IoMdArrowDropdown className="text-primary-second-dark" />
                 </div>
                 <div className="w-full bg-gray-100 rounded-full h-2">
@@ -68,39 +76,51 @@ const InfoMyPageRight = () => {
                 </div>
             </div>
             <div className="flex mt-9 mb-5 justify-between">
-                <p className="font-point text-base text-literal-normal">나의 전문 훈수는</p>
-                <Button
-                    onClick={() => (isEdit ? handleTagUpdate() : setIsEdit(true))}
-                    className={"text-xs w-13 h-6 py-1"}
-                >
-                    {isEdit ? "완료" : "수정"}
-                </Button>
+                <p className="font-point text-base text-literal-normal">
+                    {isUserMypage ? (
+                        "나의 전문 훈수는"
+                    ) : (
+                        <>
+                            <span className="font-default font-medium">' {otheruser} '</span> 님의 전문 훈수는
+                        </>
+                    )}
+                </p>
+                {isUserMypage && (
+                    <Button
+                        onClick={() => (isEdit ? handleTagUpdate() : setIsEdit(true))}
+                        className={"text-xs w-13 h-6 py-1"}
+                    >
+                        {isEdit ? "완료" : "수정"}
+                    </Button>
+                )}
             </div>
             <div className="flex flex-wrap gap-1 md:gap-3">
                 {isEdit ? (
-                    DUMMY_TAGS.map((tag, index) => (
+                    DUMMY_TAGS.filter((tag) => tag.id !== 6).map((tag) => (
                         <TagSkill
                             key={tag.id}
                             tagIcon={tag.icon}
                             tagText={tag.text}
                             isEdit={isEdit}
-                            isClicked={selectedTags.includes(index)}
-                            onClick={() => handleTagClick(index)}
+                            isClicked={selectedTags.includes(tag.id)}
+                            onClick={() => handleTagClick(tag.id)}
                         />
                     ))
                 ) : len <= 0 ? (
                     <div className="font-default text-sm text-center w-full my-2">나만의 훈수 태그를 골라보세요!</div>
                 ) : (
-                    DUMMY_TAGS.filter((_, index) => selectedTags.includes(index)).map((tag, index) => (
-                        <TagSkill
-                            key={tag.id}
-                            tagIcon={tag.icon}
-                            tagText={tag.text}
-                            isEdit={isEdit}
-                            isClicked={true}
-                            onClick={() => handleTagClick(index)}
-                        />
-                    ))
+                    DUMMY_TAGS.filter((tag) => tag.id !== 6)
+                        .filter((tag) => selectedTags.includes(tag.id))
+                        .map((tag) => (
+                            <TagSkill
+                                key={tag.id}
+                                tagIcon={tag.icon}
+                                tagText={tag.text}
+                                isEdit={isEdit}
+                                isClicked={true}
+                                onClick={() => handleTagClick(tag.id)}
+                            />
+                        ))
                 )}
             </div>
         </div>

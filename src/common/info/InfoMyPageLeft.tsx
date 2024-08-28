2b2b5af319c27f0b91677b2ca73d31f5869f236b
serim <tpfla311@naver.com>
2024-08-28 20:29:17 +0900

    Feat: 마이페이지 이미지 업로드 진행중

--- src/common/info/InfoMyPageLeft.tsx
+++ src/common/info/InfoMyPageLeft.tsx
@@ -1,29 +1,89 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../button/Button";
+import { twMerge as tw } from "tailwind-merge";
 import ProfileImage from "../profile/ProfileImage";
+import { useUserStore } from "../../config/store";
+import { userInfoImageUpdate, userInfoUpdate } from "../../api/account";
 
-const InfoMyPageLeft = () => {
+type InfoMyPageLeftProps = {
+    isUserMypage: boolean;
+};
+
+const InfoMyPageLeft = ({ isUserMypage }: InfoMyPageLeftProps) => {
     const [isEdit, setIsEdit] = useState<boolean>(false);
     const [bioText, setBioText] = useState("");
     const [nicknameText, setNicknameText] = useState("");
+    const [profileImage, setProfileImage] = useState<File | null>(null);
+    const { user, updateUser } = useUserStore();
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (user.nickname) setNicknameText(user.nickname);
+        if (user.bio) setBioText(user.bio);
+    }, [user]);
+    console.log(user.profile_image);
+
+    const handleUpdate = async () => {
+        if (!nicknameText.trim() || !bioText.trim()) {
+            setError("*별명과 한 줄 소개 모두 작성해주세요*");
+            return;
+        }
+        try {
+            await userInfoUpdate({ nickname: nicknameText, bio: bioText });
+            if (profileImage) {
+                await userInfoImageUpdate(profileImage);
+                // // 이미지 업로드 후 사용자 스토어 업데이트
+                // updateUser({ profile_image: response.data });
+            }
+        } catch (error) {
+            console.error("프로필 업데이트에 실패했습니다.", error);
+        }
+        setIsEdit(false);
+        setError(null);
+    };
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            setProfileImage(file);
+            // 이미지를 선택하면 즉시 미리보기 업데이트
+            const imageUrl = URL.createObjectURL(file);
+            updateUser({ profile_image: imageUrl }); // 미리보기 업데이트
+        }
+    };
 
     return (
-        <div className="bg-white w-full min-w-[300px] min-h-[298px] rounded-2xl px-5 py-6 flex flex-col sticky top-5">
+        <form
+            className={tw(
+                "bg-white w-full min-w-[300px] rounded-2xl px-5 py-6 flex flex-col sticky top-20",
+                isUserMypage ? "min-h-[300px]" : "min-h-[200px]"
+            )}
+        >
             <div className="flex flex-col flex-grow gap-8">
                 <div className="flex gap-5 items-center">
                     {isEdit ? (
                         <>
-                            <input type="file" id="profileImage" className="hidden" />
-                            <label htmlFor="profileImage" className="cursor-pointer w-[80px] h-[80px] flex-shrink-0">
-                                <img
-                                    src="https://dummyimage.com/80x80/000/fff"
-                                    alt="user_image"
-                                    className="rounded-full w-[80px] h-[80px] object-cover"
-                                />
+                            <input type="file" id="profileImage" className="hidden" onChange={handleImageChange} />
+                            <label
+                                htmlFor="profileImage"
+                                className="cursor-pointer w-[80px] h-[80px] flex-shrink-0 rounded-full"
+                            >
+                                <div className="relative rounded-full w-[80px] h-[80px] opacity-60 bg-black">
+                                    {user.profile_image === null ? (
+                                        <ProfileImage />
+                                    ) : (
+                                        <img
+                                            src={user.profile_image}
+                                            alt="user_image"
+                                            className="rounded-full w-[80px] h-[80px] object-cover"
+                                        />
+                                    )}
+                                    <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 rounded-full"></div>
+                                </div>
                             </label>
                         </>
                     ) : (
-                        <ProfileImage />
+                        user.profile_image === null && <ProfileImage />
                     )}
 
                     <div className="flex flex-col gap-1 flex-grow">
@@ -31,12 +91,14 @@ const InfoMyPageLeft = () => {
                         {isEdit ? (
                             <input
                                 type="text"
-                                value={nicknameText}
+                                maxLength={20}
+                                defaultValue={nicknameText}
+                                onChange={(e) => setNicknameText(e.target.value)}
                                 className="text-base text-gray-500 border border-gray-300 rounded-md px-2 py-1 focus:outline-primary-second w-full"
                             />
                         ) : (
                             <div className="text-base text-literal-normal border border-transparent rounded-md px-2 py-1">
-                                나는 훈수왕
+                                {user.nickname}
                             </div>
                         )}
                     </div>
@@ -45,23 +107,25 @@ const InfoMyPageLeft = () => {
                     <div className="text-literal-normal text-sm font-semibold px-2">한 줄 소개</div>
                     {isEdit ? (
                         <input
-                            value={bioText}
+                            defaultValue={bioText}
                             maxLength={50}
                             onChange={(e) => setBioText(e.target.value)}
                             className="text-base text-gray-500 border border-gray-300 rounded-md px-2 py-1 focus:outline-primary-second"
                         />
                     ) : (
                         <div className="text-base text-literal-normal border border-transparent rounded-md px-2 py-1">
-                            {/* 안녕하세요 최고의 훈수가가 되는 게 꿈입니다. */}
                             {bioText}
                         </div>
                     )}
                 </div>
             </div>
-            <Button color="primary" onClick={() => setIsEdit(!isEdit)}>
-                {isEdit ? "프로필 수정 완료" : "프로필 수정"}
-            </Button>
-        </div>
+            <p className="text-xs mb-1 text-center text-literal-highlight font-medium">{error}</p>
+            {isUserMypage && (
+                <Button color="primary" onClick={() => (isEdit ? handleUpdate() : setIsEdit(true))}>
+                    {isEdit ? "프로필 수정 완료" : "프로필 수정"}
+                </Button>
+            )}
+        </form>
     );
 };
 

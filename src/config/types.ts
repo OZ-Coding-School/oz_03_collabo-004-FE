be8937912d4f1e31aeeb483d1b0698ea17245499
serim <tpfla311@naver.com>
2024-08-28 20:28:07 +0900

    Feat: 유저 정보안 article 타입 추가

--- src/config/types.ts
+++ src/config/types.ts
@@ -5,13 +5,42 @@ export interface ModalProps {
 }
 
 export interface UserData {
-    article: string[];
+    articles: Article[];
     bio: string | null;
     comments: string[];
     hunsoo_level: number;
     nickname: string | null;
     profile_image: string | null;
     selected_comment_count: number;
-    selected_tags: string[];
+    selected_tags: number[];
     warning_count: number;
+    status: boolean;
+}
+
+export interface Tag {
+    tag_id: number;
+    name: string;
+}
+
+export interface Article {
+    article_id: number;
+    comments_count: number;
+    content: string;
+    created_at: string;
+    like_count: number;
+    tags: Tag[];
+    thumbnail_image: string | null;
+    title: string;
+    updated_at: string;
+    user: {
+        user_id: number;
+        nickname: string;
+    };
+    view_count: number;
+}
+
+export interface UserUpdateData {
+    nickname?: string;
+    bio?: string;
+    selected_tags?: number[];
 }

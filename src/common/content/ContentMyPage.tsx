4c12e2bfe4cb908f09970f3e7e2f4fa03cda8cfe
serim <tpfla311@naver.com>
2024-08-28 20:30:12 +0900

    Feat: api에서 받아온 데이터 보여주기

--- src/common/content/ContentMyPage.tsx
+++ src/common/content/ContentMyPage.tsx
@@ -1,27 +1,40 @@
+import { Article } from "../../config/types";
+import useFormatDate from "../../hooks/useFormatDate";
 import Badge from "../badge/Badge";
 import CommentMyPage from "../comment/CommentMyPage";
 
-const ContentMyPage = () => {
-    const tab = true;
+interface ContentMyPageProps {
+    activeTab: number;
+    article: Article;
+}
+
+const ContentMyPage = ({ activeTab, article }: ContentMyPageProps) => {
+    //! article 인지 comment인지 판단해서 데이터 넣어야함
     return (
-        <div className="max-w-[780px] min-w-[300px]">
+        <div className="max-w-[780px] min-w-[300px] my-4">
             <div className="sm:flex gap-2 mb-3 ml-2">
-                {tab && (
+                {activeTab === 1 && (
                     <p className="text-sm font-medium mr-4 text-literal-normal">따지고 보면 잔소리라 생각되진 않아</p>
                 )}
                 <div className="flex gap-2 sm:mt-0 mt-2">
-                    <p className="text-sm text-gray-600 mr-2 font-default">2024년 08월 12일</p>
-
-                    <Badge color="yellow">고민</Badge>
-                    <Badge color="yellow">소소</Badge>
+                    <p className="text-sm text-gray-600 mr-2 font-default">{useFormatDate(article.created_at)}</p>
+                    {article.tags.map((tag) => (
+                        <Badge key={tag.tag_id} color="yellow">
+                            {tag.name.slice(0, -2)}
+                        </Badge>
+                    ))}
                 </div>
             </div>
             <div className="bg-white px-6 py-4 w-full rounded-2xl">
                 <p className="text-base sm:text-lg font-default text-literal-normal pb-3 border-b border-b-gray-100">
-                    고민내용
+                    {article.title}
                 </p>
 
-                {tab ? <CommentMyPage /> : <p className="my-3 text-literal-normal text-sm">고민내용내용내용</p>}
+                {activeTab === 1 ? (
+                    <CommentMyPage />
+                ) : (
+                    <p className="my-3 text-literal-normal text-sm">고민내용내용내용</p>
+                )}
             </div>
         </div>
     );

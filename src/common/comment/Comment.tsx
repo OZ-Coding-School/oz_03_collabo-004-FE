196fc22edf492970e64bc1728fc5de2335e1939a
serim <tpfla311@naver.com>
2024-09-02 10:58:14 +0900

    Feat: 코멘트 작성 api 추가 및 코드 수정

--- src/common/comment/Comment.tsx
+++ src/common/comment/Comment.tsx
@@ -1,41 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RiEmotionUnhappyFill, RiEmotionHappyFill, RiAlarmWarningFill } from "react-icons/ri";
 import { twMerge as tw } from "tailwind-merge";
 import ModalReport from "../modal/ModalReport";
 import ModalPicture from "../modal/ModalPicture";
 import { ModalPortal } from "../../config/ModalPortal";
-import { MyComment } from "../../config/types";
+import { AiHunsu, MyComment } from "../../config/types";
+import { commenFeedback, commentSelect } from "../../api/comment";
+import { useUserStore } from "../../config/store";
 
 interface CommentProps {
     onClick?: () => void;
     className?: string;
     color?: "default" | "writer" | "ai";
     parent: string;
-    comment?: MyComment;
+    comment: MyComment;
+    ai?: AiHunsu;
 }
-
-const Comment = ({ className, color = "default", parent, comment }: CommentProps) => {
+const CommentDetail = ({ className, color = "default", parent, comment, ai }: CommentProps) => {
+    const { user } = useUserStore();
     const [isReportModalOpen, setIsReportModalOpen] = useState(false);
     const [isPictureModalOpen, setIsPictureModalOpen] = useState(false);
-    console.log(comment);
+    const [imageUrl, setImageUrl] = useState("");
+    const [helpful, setHelpful] = useState(0);
+    const [notHelpful, setNotHelpful] = useState(0);
 
-    const handleReportClick = () => {
-        setIsReportModalOpen(true);
-    };
+    useEffect(() => {
+        setHelpful(comment.helpful_count);
+        setNotHelpful(comment.not_helpful_count);
+    }, [comment.helpful_count, comment.not_helpful_count]);
 
-    const closeReportModal = () => {
-        setIsReportModalOpen(false);
-    };
+    const handleReportClick = () => setIsReportModalOpen(true);
+    const closeReportModal = () => setIsReportModalOpen(false);
 
-    const handlePictureClick = () => {
+    const handlePictureClick = (imgUrl: string) => {
         setIsPictureModalOpen(true);
+        setImageUrl(imgUrl);
     };
+    const closePictureModal = () => setIsPictureModalOpen(false);
 
-    const closePictureModal = () => {
-        setIsPictureModalOpen(false);
-    };
-
-    const formatDate = (apiDate: string | undefined | null) => {
+    const formatDate = (apiDate?: string) => {
         if (!apiDate) return;
         const date = new Date(apiDate);
 
@@ -46,20 +49,68 @@ const Comment = ({ className, color = "default", parent, comment }: CommentProps
 
         return `${month}-${day} ${hours}:${minutes}`;
     };
+
+    const formatContentWithLineBreaks = (content: string) => {
+        return content.replace(/(?:\r\n|\r|\n)/g, "<br/>");
+    };
+
+    const handleSelect = async (comment_id: number) => {
+        try {
+            const resp = await commentSelect(comment_id);
+            console.log(resp.data);
+        } catch (error) {
+            console.error("채택 실패", error);
+        }
+    };
+
+    const handleReact = async (comment_id: number, type: string) => {
+        //!이거 오바같은데
+        try {
+            const response = await commenFeedback(comment_id, type);
+            console.log(response.data.detail);
+            switch (response.data.detail) {
+                case "helpful reaction added.":
+                    setHelpful(helpful + 1);
+                    break;
+                case "helpful reaction removed.":
+                    setHelpful(helpful - 1);
+                    break;
+                case "not_helpful reaction added.":
+                    setNotHelpful(notHelpful + 1);
+                    break;
+                case "not_helpful reaction removed.":
+                    setNotHelpful(notHelpful - 1);
+                    break;
+                case "Reaction changed to not_helpful.":
+                    setNotHelpful(notHelpful + 1);
+                    setHelpful(helpful - 1);
+                    break;
+                case "Reaction changed to helpful.":
+                    setNotHelpful(notHelpful - 1);
+                    setHelpful(helpful + 1);
+            }
+        } catch (error) {
+            console.error("피드백 실패", error);
+        }
+    };
+
     return (
         <div
             className={tw(
-                "relative w-[90%]  h-full rounded-[15px] p-4 flex flex-col justify-between mb-3 ",
-                color === "default" && "bg-gray-100",
-                color === "writer" && "bg-primary-second",
-                color === "ai" && "bg-literal-info text-white",
+                "relative w-[90%] h-full rounded-[15px] p-4 flex flex-col justify-between mb-4 shadow-inner text-literal-normal",
+                color === "default" && "bg-gray-100 ",
+                color === "writer" && "bg-primary-second bg-opacity-60",
+                color === "ai" && "bg-slate-800 text-white w-full",
                 className
             )}
         >
-            <div className="absolute top-2 right-4 text-literal-highlight">{formatDate(comment?.created_at)}</div>
+            <div className={tw("absolute top-3 right-5 text-sm font-normal", color !== "ai" && "text-literal-info")}>
+                {color === "ai" ? formatDate(ai?.created_at) : formatDate(comment?.created_at)}
+            </div>
+
             <div
                 className={tw(
-                    "absolute top-8 right-8 text-literal-highlight",
+                    "absolute top-10 right-5 text-literal-highlight text-lg",
                     color === "default" && "hidden",
                     color === "writer" && "hidden"
                 )}
@@ -67,16 +118,28 @@ const Comment = ({ className, color = "default", parent, comment }: CommentProps
                 AI
             </div>
             <div className="flex flex-col gap-1">
-                <div className="flex items-center gap-2">
-                    {comment?.images && comment.images.length > 0 && (
-                        <img
-                            className="w-[45px] h-[40px] cursor-pointer"
-                            src="https://images.unsplash.com/photo-1723894960978-3f1e1cead774?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            onClick={handlePictureClick}
-                        ></img>
-                    )}
-                </div>
-                <div className="my-3 text-base px-1">{comment?.content}</div>
+                {comment.images.length > 0 && (
+                    <div className="flex gap-2 mb-1">
+                        {comment.images.map((img) => (
+                            <img
+                                key={img.id}
+                                className="w-[45px] h-[40px] cursor-pointer rounded"
+                                src={img.image}
+                                alt="Comment Image"
+                                onClick={() => handlePictureClick(img.image)}
+                            />
+                        ))}
+                    </div>
+                )}
+                {color === "ai" ? (
+                    <div
+                        className="ml-2 mr-28"
+                        dangerouslySetInnerHTML={{ __html: formatContentWithLineBreaks(ai?.content || "") }}
+                    />
+                ) : (
+                    <div className={tw("my-2 text-base px-1", color !== "default" && "mb-0")}>{comment?.content}</div>
+                )}
+
                 <div
                     className={tw(
                         "flex items-center w-[80px] cursor-pointer text-literal-highlight",
@@ -86,42 +149,68 @@ const Comment = ({ className, color = "default", parent, comment }: CommentProps
                     onClick={handleReportClick}
                 ></div>
                 <div className="flex-wrap justify-between sm:flex">
-                    <div className="flex items-center gap-1 mt-auto cursor-pointer duration-200 rounded-md px-1">
-                        <RiAlarmWarningFill className="text-literal-highlight" />
-                        <p className="text-literal-highlight font-medium text-sm" onClick={handleReportClick}>
-                            신고하기
-                        </p>
-                    </div>
+                    {color === "default" && (
+                        <div className="flex items-center gap-1 mt-auto cursor-pointer duration-200 rounded-md px-1">
+                            <RiAlarmWarningFill className="text-literal-highlight" />
+                            <p className="text-literal-highlight font-medium text-sm" onClick={handleReportClick}>
+                                신고하기
+                            </p>
+                        </div>
+                    )}
+
                     <div
                         className={tw(
-                            "flex items-center bottom-4 right-4",
+                            "flex items-center bottom-4 right-4 gap-3",
                             color === "ai" && "hidden",
                             color === "writer" && "hidden"
                         )}
                     >
-                        <div className="flex mr-3 items-center space-x-2 mt-auto">
-                            <RiEmotionUnhappyFill className="text-primary-second-dark" />
-                            <span className="text-sm">999,999</span>
-                            <RiEmotionHappyFill className="text-primary-second-dark" />
-                            <span className="text-sm">999,999</span>
+                        <div className="flex gap-3 items-center mt-auto">
+                            <div className="flex gap-1">
+                                <RiEmotionHappyFill
+                                    onClick={() => handleReact(comment.id, "helpful")}
+                                    className="text-primary-second-dark my-auto size-5 cursor-pointer duration-150 hover:scale-110 hover:text-[#ff5f1a]"
+                                />
+                                <span className="text-sm">{helpful}</span>
+                            </div>
+                            <div className="flex gap-1">
+                                <RiEmotionUnhappyFill
+                                    onClick={() => handleReact(comment.id, "not_helpful")}
+                                    className="text-primary-second-dark my-auto size-5 cursor-pointer duration-150 hover:scale-110 hover:text-[#ff5f1a]"
+                                />
+                                <span className="text-sm">{notHelpful}</span>
+                            </div>
                         </div>
-                        <button className="w-[80px] h-[30px] bg-literal-highlight text-white rounded-[5px]">
-                            채택하기
-                        </button>
+                        {comment.is_selected ? (
+                            <p className="font-normal text-literal-highlight text-lg">채택됨</p>
+                        ) : (
+                            user.user_id === comment.user && (
+                                <button
+                                    onClick={() => handleSelect(comment?.id)}
+                                    className="w-[80px] h-[30px] bg-literal-highlight text-white rounded-[5px] duration-200 hover:bg-[#a62642]"
+                                >
+                                    채택하기
+                                </button>
+                            )
+                        )}
                     </div>
                 </div>
             </div>
-
             <ModalPortal>
                 {isReportModalOpen && (
                     <ModalReport isOpen={isReportModalOpen} parent={parent} onClose={closeReportModal} />
                 )}
                 {isPictureModalOpen && (
-                    <ModalPicture isOpen={isPictureModalOpen} parent={parent} onClose={closePictureModal} />
+                    <ModalPicture
+                        isOpen={isPictureModalOpen}
+                        parent={parent}
+                        onClose={closePictureModal}
+                        imageUrl={imageUrl}
+                    />
                 )}
             </ModalPortal>
         </div>
     );
 };
 
-export default Comment;
+export default CommentDetail;

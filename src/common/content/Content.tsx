6c628771dc35038be674622345c8cb0991913761
akwjr963 <akwjr963@daum.net>
2024-09-02 11:38:06 +0900

    Content 컴포넌트 동작 기능 추가 및 ContentProps 수정

--- src/common/content/Content.tsx
+++ src/common/content/Content.tsx
@@ -1,10 +1,29 @@
-import { Article } from "../../api/article";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../../api/axios";
+import { Article } from "../../config/types";
 
 interface ContentProps {
-    article: Article;
+    articleId: number;
 }
 
-const Content = ({ article }: ContentProps) => {
+const Content = ({ articleId }: ContentProps) => {
+    const [article, setArticle] = useState<Article | null>(null);
+
+    useEffect(() => {
+        const fetchArticleDetail = async () => {
+            try {
+                const response = await axiosInstance.get<Article>(`/article/${articleId}/`, { withCredentials: true });
+                setArticle(response.data);
+            } catch (error) {
+                console.error("Failed to fetch article detail:", error);
+            }
+        };
+
+        fetchArticleDetail();
+    }, [articleId]);
+
+    if (!article) return null;
+
     return (
         <div className="flex flex-col bg-white rounded-t-lg w-[626px]">
             <div className={`flex justify-center mt-[20px] mb-[10px] ${article.thumbnail_image ? "" : "hidden"}`}>

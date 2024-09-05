7bbc61d8f704d55a1b9bc6c477643aa678b777ba
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-09-05 11:06:14 +0900

    Feat: API 호출 수정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -30,3 +30,11 @@ export const userRegister = (registerData: UserRegisterData) => {
 export const userLogin = (loginData: UserLoginData) => {
     return axiosInstance.post("/auth/login/", loginData);
 };
+
+export const userLogout = () => {
+    return axiosInstance.post("/auth/logout/");
+};
+
+export const userLoginStatus = () => {
+    return axiosInstance.get("/auth/login/status/");
+};

5b3da53fe2c5ec7f75b2646623653b0ad474454d
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2024-09-05 14:53:49 +0900

    Test: CORS 테스트

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,6 +4,5 @@ export const axiosInstance = axios.create({
     baseURL: "https://api.hunsuking.yoyobar.xyz/api",
     headers: {
         "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
     },
 });
